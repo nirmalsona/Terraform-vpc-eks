7dea61f5d12d575714720b014f0ed566cac3057c
Nirmal Sona <126976548+nirmalsona@users.noreply.github.com>
2026-03-19 02:21:36 +0530

    Delete enviroments/dev directory

--- enviroments/dev/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-eks-state-bucket-nirmal"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- enviroments/dev/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "kubernetes_version" {}
-variable "min" {}
-variable "max" {}
-variable "desired" {}
-variable "instance_types" {}
-variable "aws_region" {}
-variable "vpc_cidr" {}
-variable "env" {}
